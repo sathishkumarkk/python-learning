779a70eabd424cc0af939114db267d9dcb890563
sathishkumarkk <kksathishkumar@hotmail.com>
2024-12-27 08:37:06 +0000

    Tuples

---
+++ Dictionaries/Tuples/tuple-example.py
@@ -0,0 +1,8 @@
+airports = [
+("O’Hare International Airport", 'ORD'),
+('Los Angeles International Airport', 'LAX'),
+('Dallas/Fort Worth International Airport', 'DFW'),
+('Denver International Airport', 'DEN')
+]
+for (airport, code) in airports:
+    print('The code for {} is {}.'.format(airport, code))
